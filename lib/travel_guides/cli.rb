26535b1236a1474a76c93785535a9b264c68343b
rschlosser11 <rosalinschlosser@gmail.com>
2020-05-09 20:47:24 -0500

    added color and titles to return values

--- lib/travel_guides/cli.rb
+++ lib/travel_guides/cli.rb
@@ -2,12 +2,11 @@ class CommandLine
   @@font = TTY::Font.new(:straight)
   @@pastel = Pastel.new
   def call
-    puts @@pastel.cyan.bold(@@font.write("Welcome to travel guides!"))
+    puts @@pastel.cyan.bold(@@font.write("Welcome  to  travel  guides!"))
     puts ""
     puts "Please wait while we load the regions to which you can travel:"
     self.display_continents
-    puts ""
-    puts "Please type the name of the region you want to visit or type 'exit'."
+    puts "Please type the number of the region you want to visit or type 'exit'."
     self.run
   end
 
@@ -19,21 +18,22 @@ class CommandLine
   end
 
   def continent_menu(continent_input, country_input, last_input)
-    @continent_input = gets.strip.split(" ").map{|word| word.capitalize}.join(" ")
+    continent_num = gets.strip.to_i
+    @continent_input = Continent.all[continent_num - 1].name
     continent_input = @continent_input
     country_input = @country_input
     last_input = @last_input
     if @continent_input.downcase == 'exit'
+      puts ""
       self.leave
-    elsif Continent.find_by_name(@continent_input)
+    elsif Continent.find_by_name(continent_input)
       display_countries(@continent_input)
-      puts ""
       puts "Please type the name of the country/state that you'd like to explore\n or type 'regions' to return to the region list \n or type 'exit'"
-      puts ""
       @country_input = gets.strip.split(" ").map{|word| word.capitalize}.join(" ")
       self.country_menu(continent_input, country_input, last_input)
     else
-      puts "Please type the name of a valid region or 'exit'."
+      puts ""
+      puts @@pastel.red("Please type the name of a valid region or 'exit'.")
       self.continent_menu(continent_input, country_input, last_input)
     end
   end
@@ -44,10 +44,10 @@ class CommandLine
     last_input = @last_input
     if @country_input.downcase == 'regions'
       self.display_continents
-      puts ""
-      puts "Please type the name of the region you want to visit or type 'exit'."
+      puts "Please type the number of the region you want to visit or type 'exit'."
       puts ""
       self.continent_menu(continent_input, country_input, last_input)
+      puts ""
     elsif @country_input.downcase == 'exit'
       self.leave
     elsif Country.find_by_name(@country_input)
@@ -56,7 +56,8 @@ class CommandLine
       @last_input = gets.strip.downcase
       self.last_menu(continent_input, country_input, last_input)
     else
-      puts "Please type the name of a country/state from the list."
+      puts ""
+      puts @@pastel.red("Please type the name of a country/state from the list.")
       @country_input = gets.strip.split(" ").map{|word| word.capitalize}.join(" ")
       puts ""
       self.country_menu(continent_input, country_input, last_input)
@@ -72,7 +73,7 @@ class CommandLine
     elsif @last_input == 'regions'
       self.display_continents
       puts ""
-      puts "Please type the name of the region you want to visit or type 'exit'."
+      puts "Please type the number of the region you want to visit or type 'exit'."
       self.continent_menu(continent_input, country_input, last_input)
     elsif @last_input == 'countries'
       display_countries(continent_input)
@@ -83,7 +84,7 @@ class CommandLine
       self.country_menu(continent_input, country_input, last_input)
     else
       puts ""
-      puts "Please enter 'exit', 'regions', or 'countries'!"
+      puts @@pastel.red("Please enter 'exit', 'regions', or 'countries'!")
       @last_input = gets.strip.downcase
       self.last_menu(continent_input, country_input, last_input)
     end
@@ -93,9 +94,17 @@ class CommandLine
     # check to see if continents created already
     if Continent.all.length == 0
       Scraper.get_continents
-      Continent.all.each {|continent| puts continent.name}
+      puts ""
+      puts "--------------------------"
+      Continent.all.each_with_index {|continent, index| puts "#{@@pastel.cyan(index + 1)}. #{@@pastel.cyan(continent.name)}"}
+      puts "--------------------------"
+      puts ""
     else
-      Continent.all.each {|continent| puts continent.name}
+      puts ""
+      puts "--------------------------"
+      Continent.all.each_with_index {|continent, index| puts "#{@@pastel.cyan(index + 1)}. #{@@pastel.cyan(continent.name)}"}
+      puts "--------------------------"
+      puts ""
     end
   end
 
@@ -104,16 +113,16 @@ class CommandLine
     index = 0
     while index < countries.length
       if index % 3 == 0 && index != 0 && index != countries.length - 1
-        puts "#{countries[index - 3].name.ljust(30)}   #{countries[index -2].name.ljust(30)}   #{countries[index - 1].name.ljust(30)}"
+        puts "#{@@pastel.cyan(countries[index - 3].name.ljust(30))}   #{@@pastel.cyan(countries[index -2].name.ljust(30))}   #{@@pastel.cyan(countries[index - 1].name.ljust(30))}"
       elsif countries.length == 1
-        puts "#{countries[index].name.ljust(30)}"
+        puts "#{@@pastel.cyan(countries[index].name.ljust(30))}"
       elsif index == countries.length - 1 && index % 3 == 0
-        puts "#{countries[index - 3].name.ljust(30)}   #{countries[index -2].name.ljust(30)}   #{countries[index - 1].name.ljust(30)}"
-        puts "#{countries[index].name.ljust(30)}"
+        puts "#{@@pastel.cyan(countries[index - 3].name.ljust(30))}   #{@@pastel.cyan(countries[index -2].name.ljust(30))}   #{@@pastel.cyan(countries[index - 1].name.ljust(30))}"
+        puts "#{@@pastel.cyan(countries[index].name.ljust(30))}"
       elsif index == countries.length - 1 && index % 2 == 0
-        puts "#{countries[index - 1].name.ljust(30)}   #{countries[index].name.ljust(30)}"
+        puts "#{@@pastel.cyan(countries[index - 1].name.ljust(30))}   #{@@pastel.cyan(countries[index].name.ljust(30))}"
       elsif index == countries.length - 1 && index % 2 == 1
-        puts "#{countries[index].name.ljust(30)}"
+        puts "#{@@pastel.cyan(countries[index].name.ljust(30))}"
       end
       index += 1
     end
@@ -123,19 +132,30 @@ class CommandLine
     continent = Continent.all.find{|continent| continent.name == input}
     # NEED TO Check to see if the continent's countries have been called already
     if Country.find_by_continent(continent).length == 0
-      puts "Please wait while we generate a list of countries you can visit. This may take a bit depending on the region!"
       puts ""
+      puts "Please wait while we generate a list of countries you can visit. This may take a bit depending on the region!"
       # puts "#{continent.name}"
       Scraper.get_countries(continent)
+      puts ""
+      puts "--------------------------"
+      puts "#{@@pastel.cyan.bold(@@font.write(@continent_input))}"
       display_countries_columns(continent)
+      puts "--------------------------"
+      puts ""
     else
       puts ""
+      puts "--------------------------"
+      puts "#{@@pastel.cyan.bold(@@font.write(@continent_input))}"
       display_countries_columns(continent)
+      puts "--------------------------"
+      puts ""
     end
   end
 
   def leave
-    puts "Thank you for exploring Travel Guides! See you for your next adventure!"
+    puts ""
+    puts @@pastel.cyan.bold("Thank you for exploring Travel Guides! See you for your next adventure!")
+    puts ""
     exit
   end
 
@@ -146,7 +166,10 @@ class CommandLine
       travel_info = travel_info.insert(travel_info.index(/[\.!?]\w/) + 1, "\n\n")
     end
     puts ""
-    puts WordWrap.ww "#{travel_info}", 100
+    puts "--------------------------"
+    puts "#{@@pastel.cyan.bold(@@font.write(country_input.upcase))}"
+    puts WordWrap.ww "#{@@pastel.cyan(travel_info)}", 100
+    puts "--------------------------"
     puts ""
   end
 end
